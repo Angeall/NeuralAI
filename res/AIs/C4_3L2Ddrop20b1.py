142a7e942c1bb09e048d21028876c90d8e13e0c0
Anthony Rouneau <angeal1105@gmail.com>
2017-04-28 02:36:21 +0200

    new AIs added

---
+++ res/AIs/C4_3L2Ddrop20b1.py
@@ -0,0 +1,14 @@
+try:
+    from res.AIs.neuralconnect4 import NeuralConnect4
+except ModuleNotFoundError:
+    from .neuralconnect4 import NeuralConnect4
+
+
+class _3L2DNoDrop(NeuralConnect4):
+    @property
+    def _modelName(self) -> str:
+        return "C4_3L2Ddrop20b1.h5"
+
+    @property
+    def _batchSize(self) -> int:
+        return 1
