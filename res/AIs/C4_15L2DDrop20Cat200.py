873cf3aa4063601bea56d1778764b9409564ca16
Anthony Rouneau <angeal1105@gmail.com>
2017-05-05 18:13:58 +0200

    added network

---
+++ res/AIs/C4_15L2DDrop20Cat200.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+try:
+    from neuralconnect4 import NeuralConnect4
+except ModuleNotFoundError:
+    from .neuralconnect4 import NeuralConnect4
+
+
+class _15L2DDrop20Cat200(NeuralConnect4):
+
+    def _decodeFromFloat(self, sequence: np.ndarray) -> np.ndarray:
+        actions = []
+        sequence = sequence[0]
+        for lst in sequence:
+            actions.append(np.argmax(lst) - 1)
+        return np.array([actions])
+
+    @property
+    def _modelName(self) -> str:
+        return "C4_15L2DDrop20Cat200.h5"
+
+    @property
+    def _batchSize(self) -> int:
+        return 1
