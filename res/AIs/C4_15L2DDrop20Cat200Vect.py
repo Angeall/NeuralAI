d2f76706f7328567dd169c061ac5aac9d57e76ab
Anthony Rouneau <angeal1105@gmail.com>
2017-05-06 11:49:51 +0200

    Added networks

---
+++ res/AIs/C4_15L2DDrop20Cat200Vect.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+try:
+    from neuralconnect4 import NeuralConnect4
+except ModuleNotFoundError:
+    from .neuralconnect4 import NeuralConnect4
+
+
+class _15L2DDrop20Cat200Vect(NeuralConnect4):
+
+    def _encodeToFloat(self, sequence: np.ndarray) -> np.ndarray:
+        sequence += 1
+        encoded_seqs = []
+        for features in sequence:
+            encoded_seq = [0 for i in range(16)]
+            encoded_seq[int(features[0])] += 1
+            encoded_seq[int(features[1]) + 8] += 1
+            encoded_seqs.append(encoded_seq)
+        return np.array(encoded_seqs)
+
+    def _decodeFromFloat(self, sequence: np.ndarray) -> np.ndarray:
+        actions = []
+        sequence = sequence[0]
+        for lst in sequence:
+            actions.append(np.argmax(lst) - 1)
+        return np.array([actions])
+
+    @property
+    def _modelName(self) -> str:
+        return "C4_15L2DDrop20Cat200Vect.h5"
+
+    @property
+    def _batchSize(self) -> int:
+        return 1
---
+++ res/AIs/connect4_hmm_pl1.py
@@ -0,0 +1,40 @@
+import random
+
+import numpy as np
+
+from pytgf.examples.connect4.controllers import Connect4BotPlayer
+from pytgf.examples.connect4.rules import Connect4API
+
+try:
+    from hmmbot import HMMBot
+except ModuleNotFoundError:
+    from .hmmbot import HMMBot
+
+
+class Connect4HMM(Connect4BotPlayer, HMMBot):
+    @property
+    def _modelName(self) -> str:
+        return "c4hmm_player1.bin"
+
+    def _encodeToOneHot(self, sequence: list) -> np.ndarray:
+        encoded_sequence_x = []
+        for features in sequence:
+            features = [features[0] + 1, features[1] + 1]
+            feature_idx = (features[0] * 8) + features[1]
+            encoded_features = [1 if j == feature_idx else 0 for j in range(64)]
+            encoded_sequence_x.append(encoded_features)
+        return np.array(encoded_sequence_x)
+
+    def _selectNewMove(self, game_state: Connect4API):
+        winning_move = game_state.getDirectWinningMove(self.playerNumber)
+        if winning_move is not None:
+            return winning_move
+        losing_move = game_state.getDirectLosingMove(self.playerNumber)
+        if losing_move is not None:
+            return losing_move  # Block the opponent
+        move = super()._selectNewMove(game_state)
+        succeeded, _ = self.gameState.simulateMove(self.playerNumber, move)
+        while not succeeded:
+            move = random.choice(self.possibleMoves)
+            succeeded, _ = self.gameState.simulateMove(self.playerNumber, move)
+        return move
