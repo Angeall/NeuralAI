4d152d7322a17d565fa2804a80846423c4a35755
Anthony Rouneau <angeal1105@gmail.com>
2017-05-08 16:57:51 +0200

    added network

--- reinforcement_c4.py
+++ reinforcement_c4.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
+from pytgf.controls.controllers import Bot
 from pytgf.data.gatherer import Gatherer
 from pytgf.data.routines import ReinforcementRoutine
 from pytgf.controls.controllers.passive import Passive
@@ -20,11 +21,17 @@ if __name__ == "__main__":
     cls1 = get_class(path_1, file_name_1)
     cls2 = get_class(path_2, file_name_2)
 
-    own_controller = cls1(1)
-    opponent_controllers = [cls2(2)]
-    battle = ReinforcementRoutine(own_controller, opponent_controllers, Gatherer([]), tuple(range(7)),
-                                  lambda api: {player: 100 * api.hasWon(player) for player in (1, 2)})
-
     core = create_game({1: Passive, 2: Passive}, 2, 2, False).game
     state = Connect4API(core)
-    battle.routine(1, state)
+
+    own_controller = cls1(1)  # type: Bot
+    own_controller.gameState = core
+    opponent_controllers = [cls2(2)]
+    opponent_controllers[0].gameState = core
+    battle = ReinforcementRoutine(own_controller, opponent_controllers, Gatherer([]), tuple(range(7)),
+                                  lambda api: {player: 100 * api.hasWon(player) for player in (1, 2)},
+                                  must_write_files=False, must_keep_temp_files=False, min_end_states=2,
+                                  min_victories=5)
+
+    test = battle.routine(1, state)
+    print(test, '\n')
