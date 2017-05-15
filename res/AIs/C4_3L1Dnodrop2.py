d63801a254f87500a8aab518a2f4f88009bd1a4c
Anthony Rouneau <angeal1105@gmail.com>
2017-05-15 17:55:19 +0200

    added network

--- res/AIs/C4_3L1Dnodrop2.py
+++ res/AIs/C4_3L1Dnodrop2.py
@@ -4,11 +4,11 @@ except ModuleNotFoundError:
     from .neuralconnect4 import NeuralConnect4
 
 
-class _3L2DNoDrop(NeuralConnect4):
+class _3L1DNoDrop2(NeuralConnect4):
     @property
     def _modelName(self) -> str:
-        return "C4_3L2Dnodrop.h5"
+        return "C4_3L1Dnodrop2.h5"
 
     @property
     def _batchSize(self) -> int:
-        return 1
+        return 20
