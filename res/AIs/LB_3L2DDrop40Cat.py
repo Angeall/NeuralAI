cf516d6e5ba408c0b171e4abfe08d0b2b5b4d7cb
Anthony Rouneau <angeal1105@gmail.com>
2017-05-21 20:44:50 +0200

    added categorical network for lb

---
+++ res/AIs/LB_3L2DDrop40Cat.py
@@ -0,0 +1,22 @@
+try:
+    from neurallazerbike import NeuralLazerbike
+except ModuleNotFoundError:
+    from .neurallazerbike import NeuralLazerbike
+
+
+class LB_3L2DDrop40Cat(NeuralLazerbike):
+
+    def _decodeFromFloat(self, sequence: np.ndarray) -> np.ndarray:
+        actions = []
+        sequence = sequence[0]
+        for lst in sequence:
+            actions.append(np.argmax(lst) - 2)
+        return np.array([actions])
+
+    @property
+    def _modelName(self) -> str:
+        return "LB_3L2DDrop40Cat.h5"
+
+    @property
+    def _batchSize(self) -> int:
+        return 1
